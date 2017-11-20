90399bf35704619104a1430b4b82ab31ae9bdf2f
Neville <ndwadia@gmail.com>
2017-11-20 16:06:51 +0530

    added mapValues to get avg

--- src/python/PerKeyAvg.py
+++ src/python/PerKeyAvg.py
@@ -17,14 +17,13 @@ def perKeyAvg(nums):
     sumCount = nums.combineByKey((lambda x: (x, 1)),
                                  (lambda x, y: (x[0] + y, x[1] + 1)),
                                  (lambda x, y: (x[0] + y[0], x[1] + y[1])))
-    return sumCount.collectAsMap()
+    return sumCount.mapValues(lambda xy: xy[0]/xy[1]).collectAsMap()
 
 if __name__ == "__main__":
     master = "local"
     if len(sys.argv) == 2:
         master = sys.argv[1]
     sc = SparkContext(master, "Sum")
-    nums = sc.parallelize(
-        [("coffee", 1), ("pandas", 2), ("coffee", 3), ("very", 4)])
+    nums = sc.parallelize([("coffee", 1), ("pandas", 2), ("coffee", 3), ("very", 4)])
     avg = perKeyAvg(nums)
     print avg
